9ab77cc6ccacd2e665a957b221a30faa27560ba6
Vlad Vovk <vovk.vladyslav94@gmail.com>
2020-04-27 17:11:21 +0300

    BE - Fix CORS

--- favorite-movies-be/app.js
+++ favorite-movies-be/app.js
@@ -7,6 +7,12 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+  
 app.use('/api/movies', require('./routes/movies.routes'))
 
 async function appStart () {
